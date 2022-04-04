a4e9cbf9b26d30265506d607f1bde1152e8827d9
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-04-04 08:12:17 +0000

    ArrayLists 1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,25 @@
 package com.cc.java;
 
+import java.util.ArrayList;
+
 public class App {
     
     public static void main(String[] args) {
      
+        // Order order = new Order("order_1");
+        // System.out.println(order.getName());
+
+        ArrayList<Order> orders = new ArrayList<>();
+        System.out.println("Orders: " + orders);
+        orders.add(new Order("Auftrag_1"));
+        System.out.println("Orders: " + orders);
+        orders.add(new Order("Auftrag_2"));
+        
+        System.out.println("Orders: " + orders);
+        System.out.println(orders.get(0));
+        System.out.println(orders.get(orders.size()-1));
+        
+
     }
 
 }
