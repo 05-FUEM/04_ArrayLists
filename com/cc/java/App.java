78021982bd1a029029fb0216054043e23bf1d098
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-04-04 08:18:05 +0000

    ArrayLists 2 / for

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -10,13 +10,17 @@ public class App {
         // System.out.println(order.getName());
 
         ArrayList<Order> orders = new ArrayList<>();
+        // System.out.println("Orders: " + orders);
+        // orders.add(new Order("Auftrag_1"));
+        // System.out.println("Orders: " + orders);
+        // orders.add(new Order("Auftrag_2"));
+
+        for (int i = 0; i < 4; i++) {
+            orders.add(new Order("Auftrag_" + i));
+        }
+
         System.out.println("Orders: " + orders);
-        orders.add(new Order("Auftrag_1"));
-        System.out.println("Orders: " + orders);
-        orders.add(new Order("Auftrag_2"));
-        
-        System.out.println("Orders: " + orders);
-        System.out.println(orders.get(0));
+        System.out.println(orders.get(0).getName());
         System.out.println(orders.get(orders.size()-1));
         
 
